92c22ad8ea91411f1a6df81961dc89cac50c0360
Anam-Mushtaq <80307413+Anam-Mushtaq@users.noreply.github.com>
2021-03-27 17:06:48 +0500

    Add files via upload

---
+++ assimnt 4/q60.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace q60
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("expresion for floating point:");
+            Console.WriteLine(21 / 4.0);
+
+            Console.WriteLine("expression for integer quotient");
+            Console.WriteLine(21 / 4);
+
+            Console.WriteLine("expression for remainder");
+            Console.WriteLine(21 % 4);
+        }
+
+    }
+}
